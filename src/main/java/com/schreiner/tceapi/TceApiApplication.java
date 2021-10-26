eccb4bd315a42bdaafdd12f1957c13202512e5de
Daniel Schreiner <schreiner.daniel@gmail.com>
2021-10-26 03:46:04 -0300

    First Commit

---
+++ src/main/java/com/schreiner/tceapi/TceApiApplication.java
@@ -0,0 +1,13 @@
+package com.schreiner.tceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schreiner/tceapi/TceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schreiner.tceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
